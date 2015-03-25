a27e07f8f24904bc1b8d87e616801f690d62b3f4
seafoam6 <steve@stevebarman.com>
2015-03-25 19:13:09 -0400

    BINGO COMPLETE v1.0

---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+  var squareContent = [
+    "Jim Pool censors an anti-racism post",
+    "Taxes!",
+    "Rats!",
+    "Those jerks at city hall",
+    "Racism",
+    "Water Bill",
+    "Electric Bill",
+    "Something they should have Googled",
+    "Being mean about neighbor",
+    "Dog without a leash",
+    "Post office complaints",
+    "Dream Cruise",
+    "Complete Nonsense",
+    "Good Chinese/Thai/Sushi recommendation",
+    "Pizza",
+    "Call the DPW",
+    "There's too many restaurants in Ferndale",
+    "blatant attention seeking",
+    "need roofer",
+    "need plumber",
+    "buy/sell car",
+    "food truck",
+    "bring child into bar",
+    "post from non-ferndale resident",
+    "Actually useful post (The Unicorn)",
+    "Free stuff"
+  ];
+  console.log("there are "+squareContent.length+" choices");
+  //+ Jonas Raoni Soares Silva
+        //@ http://jsfromhell.com/array/shuffle [v1.0]
+    function shuffle(o){ //v1.0
+        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    };
+  var squares = $('.square');
+
+  squareContent = shuffle(squareContent);
+
+    squares.each(function(){
+        $(this).html(squareContent.pop());
+        
+    });
+
+
+
