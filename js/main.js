62d6575dac78cc90894c781affaeafae38b3bb17
Jim McLuckie <jim@bigdealinc.co>
2015-03-25 22:04:25 -0400

    Change square when clicked
    Changes the background color of the square when clicked, from grey to
    red, red to grey. Cleaned up formatting too since I’m anal like that.

--- js/main.js
+++ js/main.js
@@ -1,47 +1,64 @@
 
-  var squareContent = [
-    "Jim Pool censors an anti-racism post",
-    "Taxes!",
-    "Rats!",
-    "Those jerks at city hall",
-    "Racism",
-    "Water Bill",
-    "Electric Bill",
-    "Something they should have Googled",
-    "Being mean about neighbor",
-    "Dog without a leash",
-    "Post office complaints",
-    "Dream Cruise",
-    "Complete Nonsense",
-    "Good Chinese/Thai/Sushi recommendation",
-    "Pizza",
-    "Call the DPW",
-    "There's too many restaurants in Ferndale",
-    "blatant attention seeking",
-    "need roofer",
-    "need plumber",
-    "buy/sell car",
-    "food truck",
-    "bring child into bar",
-    "post from non-ferndale resident",
-    "Actually useful post (The Unicorn)",
-    "Free stuff"
-  ];
-  console.log("there are "+squareContent.length+" choices");
-  //+ Jonas Raoni Soares Silva
+var squareContent = [
+	"Jim Pool censors an anti-racism post",
+	"Taxes!",
+	"Rats!",
+	"Those jerks at city hall",
+	"Racism",
+	"Water Bill",
+	"Electric Bill",
+	"Something they should have Googled",
+	"Being mean about neighbor",
+	"Dog without a leash",
+	"Post office complaints",
+	"Dream Cruise",
+	"Complete Nonsense",
+	"Good Chinese/Thai/Sushi recommendation",
+	"Pizza",
+	"Call the DPW",
+	"There's too many restaurants in Ferndale",
+	"blatant attention seeking",
+	"need roofer",
+	"need plumber",
+	"buy/sell car",
+	"food truck",
+	"bring child into bar",
+	"post from non-ferndale resident",
+	"Actually useful post (The Unicorn)",
+	"Free stuff"
+];
+
+console.log("there are "+squareContent.length+" choices");
+  
+//+ Jonas Raoni Soares Silva
         //@ http://jsfromhell.com/array/shuffle [v1.0]
-    function shuffle(o){ //v1.0
-        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-        return o;
-    };
-  var squares = $('.square');
+function shuffle(o) { 
+	//v1.0
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
 
-  squareContent = shuffle(squareContent);
+var squares = $('.square');
 
-    squares.each(function(){
-        $(this).html(squareContent.pop());
-        
-    });
+squareContent = shuffle(squareContent);
+
+squares.each(function(){
+    $(this).html(squareContent.pop());
+});
+
+
+$(".square").click(function() {
+
+	if ($(this).css("background-color") == "rgb(248, 248, 255)") {
+		$(this).css({ 
+			"background": "red",
+			"color": "white" });
+	} else {
+		$(this).css({ 
+			"background": "#F8F8FF",
+			"color": "black" });		
+	}
+});
 
 
 
